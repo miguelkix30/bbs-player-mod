628e13a98ca644ea4cb4a9951946e65aff442534
McHorse <mchorselessrider@gmail.com>
2024-07-23 21:33:30 +0100

    Fix fake player with Sinytra Connector

--- src/main/java/mchorse/bbs_mod/actions/SuperFakePlayerNetworkHandler.java
+++ src/main/java/mchorse/bbs_mod/actions/SuperFakePlayerNetworkHandler.java
@@ -3,36 +3,21 @@ package mchorse.bbs_mod.actions;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.NetworkSide;
 import net.minecraft.network.PacketCallbacks;
-import net.minecraft.network.listener.PacketListener;
 import net.minecraft.network.packet.Packet;
-import net.minecraft.server.network.ConnectedClientData;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 public class SuperFakePlayerNetworkHandler extends ServerPlayNetworkHandler
 {
-    private static final ClientConnection FAKE_CONNECTION = new FakeClientConnection();
+    private static final ClientConnection FAKE_CONNECTION = new ClientConnection(NetworkSide.CLIENTBOUND);
 
     public SuperFakePlayerNetworkHandler(ServerPlayerEntity player)
     {
-        super(player.getServer(), FAKE_CONNECTION, player, ConnectedClientData.createDefault(player.getGameProfile()));
+        super(player.getServer(), FAKE_CONNECTION, player);
     }
 
     @Override
-    public void send(Packet<?> packet, @Nullable PacketCallbacks callbacks)
+    public void sendPacket(Packet<?> packet, @Nullable PacketCallbacks callbacks)
     {}
-
-    private static final class FakeClientConnection extends ClientConnection
-    {
-        private FakeClientConnection()
-        {
-            super(NetworkSide.CLIENTBOUND);
-        }
-
-        @Override
-        public void setPacketListener(PacketListener packetListener)
-        {}
-    }
 }
